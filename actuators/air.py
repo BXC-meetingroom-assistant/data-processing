396ca407d2d4e9f9b8cd39d09166e2a4f12e4a9a
Sergey Melnik <sergey.melnik@commercetools.de>
2017-03-15 17:21:31 +0100

    Interface for airwasher

---
+++ actuators/air.py
@@ -0,0 +1,24 @@
+import requests
+
+urls = {
+    'on': 'https://maker.ifttt.com/trigger/bad_air/with/key/nTlnK3gknAAXc0meB96ot1hidkPnyEJnuX2BrSO6CXz',
+    'off': 'https://maker.ifttt.com/trigger/good_air/with/key/nTlnK3gknAAXc0meB96ot1hidkPnyEJnuX2BrSO6CXz'
+}
+
+
+def check_status_code(url):
+    request = requests.get(url)
+    if request.status_code == '200':
+        return True
+    else:
+        return False
+
+    pass
+
+
+def enable():
+    return(check_status_code(urls['on']))
+
+
+def disable():
+    return(check_status_code(urls['off']))
