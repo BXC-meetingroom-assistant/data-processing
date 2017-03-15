4cc3f566e3b4cecb3125c5876d450ae3f2051c9f
Sergey Melnik <sergey.melnik@commercetools.de>
2017-03-15 16:12:34 +0100

    Read settings from file

--- influx-test.py
+++ influx-test.py
@@ -1,18 +1,21 @@
 from influxdb import InfluxDBClient
+import json
 
-client = InfluxDBClient('bcx-workhorse.bosch-iot-suite.com', 8086, '', '', 'bcx2017_telemetry')
+with open('settings.json') as data_file:
+    settings = json.load(data_file)
+
+
+client = InfluxDBClient(
+    settings['server'],
+    settings['port'],
+    settings['user'],
+    settings['password'],
+    settings['namespace']
+)
 
 result = client.query('SELECT * from "netatmo.IAQ02" order by time desc limit 10;')
 
-# print(result)
-
-# print("Result: {0}".format(result))
-print("Result: {0}".format(result.get_points()))
-
 for point in result.get_points():
-    # print(point.keys())
+    print(point.keys())
     for item in point.items():
         print(item)
-        # if key == 'value.dashboard_data.properties.CO2':
-            # print(point.items()['value.dashboard_data.properties.CO2'])
-    exit(0)
